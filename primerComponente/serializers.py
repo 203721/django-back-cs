dc916ec675aef2ff5403b24205359b5f6f07bc43
203721 <203721@ids.upchiapas.edu.mx>
2022-01-19 18:22:11 -0600

    create first models, serializers, views and urls

---
+++ primerComponente/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import routers, serializers, viewsets
+
+# import of models
+from primerComponente.models import PrimerTabla
+
+class PrimerTablaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PrimerTabla
+        fields = ('nombre','edad')
--- primerComponente/views.py
+++ primerComponente/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# imports of aggregate models
+from primerComponente.models import PrimerTabla
+
+# serializer imports
+from primerComponente.serializers import PrimerTablaSerializer
 
 # Create your views here.
+class PrimerTablaList(APIView):
+    def get(self, request, format=None):
+        queryset = PrimerTabla.objects.all()
+        serializer = PrimerTablaSerializer(queryset, many=True, context={'request':request})
+        return Response(serializer.data)
